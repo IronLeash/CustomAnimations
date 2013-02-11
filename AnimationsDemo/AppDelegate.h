aaf19f46c05fbbee869d049cd2eb5ea727b4dc73
Ilker Baltaci <ilkerbaltaic@gmx.at>
2013-02-11 12:37:10 +0100

    Initial Commit

---
+++ AnimationsDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AnimationsDemo
+//
+//  Created by Ilker Baltaci on 2/11/13.
+//  Copyright (c) 2013 Ilker Baltaci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AnimationsDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnimationsDemo
+//
+//  Created by Ilker Baltaci on 2/11/13.
+//  Copyright (c) 2013 Ilker Baltaci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
